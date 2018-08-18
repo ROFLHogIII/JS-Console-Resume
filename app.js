e66c7527eb1bcf4292deffdc04789f14bcc59f13
Richard Garner <richardgarner@mac.com>
2018-08-18 10:30:53 -0500

    10:30 A.M. skill set added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var n = "Richard Garner";
 var uN = n.toUpperCase();
 console.log('Name: ' + uN);
+console.log('Career: Intergalactic Warlord');
+console.log('Description: WRITHE, PUNY APES, AND WITNESS MY MAJESTY');
 
 var amazon = {
     comp: "Amazon",
     job: "Packer",
-    desc: "I touched other people's packages",
+    desc: "I touched other people's packages. ;)",
 };
 var costco = {
     comp: "Costco",
@@ -15,18 +17,72 @@ var costco = {
 var papa = {
     comp: "Papa Johns",
     job: "Delivery Boy",
-    desc: "I delivered pizza to people who never tipped for a wadge that should be illigal.",
+    desc: "I delivered pizza to people who never tipped for a wage that should be illegal.",
 };
 var work = [amazon, costco, papa];
 
-console.log('My Previous Experiance')
+console.log('My Previous Experience:')
 for (let i in work) {
     displayPosition(work[i].comp, work[i].job, work[i].desc)
 };
 
+var skill1 = {
+    name: 'Tactical Genius',
+    cool: 1,
+};
+var skill2 = {
+    name: 'Expert video game player',
+    cool: 1,
+};
+var skill3 = {
+    name: 'Magic: the Gathering',
+    cool: 0,
+};
+var skill4 = {
+    name: 'Pearless warrior',
+    cool: 0,
+};
+var skill5 = {
+    name: 'Ruler of 1000 star systems',
+    cool: 0,
+};
+var skill6 = {
+    name: 'Can make a mean fettuchini alfredo',
+    cool: 1,
+};
+var skill7 = {
+    name: 'Able to decimate armies with a single swing of my blade',
+    cool: 1,
+};
+var skill8 = {
+    name: 'D&D proficianato',
+    cool: 1,
+};
+var skill9 = {
+    name: 'Plays a little piano',
+    cool: 0,
+};
+var skill10 = {
+    name: 'Basic js coding',
+    cool: 1,
+};
+var skills = [skill1, skill2, skill3, skill4, skill5, skill6, skill7, skill8, skill9, skill10];
 
+console.log('My Skills:')
+for (let i in skills) {
+    displaySkills(skills[i].name, skills[i].cool);
+};
+
+function displaySkills(name, cool) {
+    if (cool === 1) {
+        console.log('* BAM: ' + name)
+    } else {
+        console.log('* ' + name)
+    }
+}
 
 function displayPosition(comp, job, desc) {
-    console.log(job + ' at ' + comp + ' - ' + desc)
+    console.log('* ' + job + ' at ' + comp + ' - ' + desc)
 };
-console.log(work)
+console.log(work);
+console.log(skills);
